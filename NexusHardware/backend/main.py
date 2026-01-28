08079914c8414d5834e6b4e495672c117398059c
leandrosebastiangraneros <kuazzarotaku@gmail.com>
2026-01-27 21:26:44 -0300

    Fix: Configurar CORS abierto y conexion a backend desplegado

--- NexusHardware/backend/main.py
+++ NexusHardware/backend/main.py
@@ -16,9 +16,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # Vite Local
-        "http://127.0.0.1:5173",  # Vite Local IP
-        "https://leandrograneros.github.io"  # Production Frontend
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
